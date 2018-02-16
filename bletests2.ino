d94c0eb23376fd8bca832535bb1ac10101bea581
seti97 <36262292+seti97@users.noreply.github.com>
2018-02-16 11:59:17 +0100

    Add files via upload

---
+++ bletests2.ino
@@ -0,0 +1,36 @@
+#include <SPI.h>
+#include <Boards.h>
+#include <RBL_nRF8001.h>
+#include <services.h>
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+#include <Arduino.h>
+
+// This function is called only once, at reset.
+void setup()
+{
+    // Enable serial debug.
+    Serial.begin(9600);
+    Serial.println("Arduino Beacon example started");
+    Serial.println("Serial rate set to 9600");
+
+    // Set a custom BLE name for the beacon.
+    // Note that each Arduino should be given a unique name!
+   
+   // ble_set_name("classroom1");
+
+    // Initialize BLE library.
+    ble_begin();
+    Serial.println("Beacon activated");
+
+}
+
+// This function is called continuously, after setup() completes.
+void loop()
+{
+    // Process BLE events.
+    ble_do_events();
+
+}
+
